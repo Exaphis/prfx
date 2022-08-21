e81ebf1aaf0f587ff4fbc58eac5bba83b9be7969
Kevin Wu <kevin@kevinniuwu.com>
2022-08-20 23:09:08 -0400

    Implement GET prefix

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,68 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Router } from "itty-router";
+import { error, json, withContent } from "itty-router-extras";
+
+const router = Router();
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  PRFX_KV: KVNamespace;
 }
 
+router.get("/:prefix", async ({ params }, env: Env, ctx: ExecutionContext) => {
+  const prefix = params?.prefix;
+  if (prefix === undefined) {
+    return error(400, "missing prefix");
+  }
+
+  // try to save a list operation if possible
+  const value = await env.PRFX_KV.get(prefix);
+  if (value) {
+    return json({
+      data: {
+        key: prefix,
+        value: value,
+      },
+    });
+  }
+
+  // otherwise, do a full list operation
+  const listResult = await env.PRFX_KV.list<string>({ prefix: prefix });
+  if (listResult.keys.length === 0) {
+    return error(404, "no keys found");
+  }
+
+  if (listResult.keys.length === 1) {
+    return json({
+      data: {
+        key: listResult.keys[0].name,
+        value: await env.PRFX_KV.get(listResult.keys[0].name),
+      },
+    });
+  }
+  return json({
+    data: {
+      items: listResult.keys.map((key) => key.name),
+    },
+  });
+});
+
+router.put(
+  "/:key",
+  withContent,
+  async (request, env: Env, ctx: ExecutionContext) => {
+    console.log(request);
+    console.log(await request.json());
+    // TODO: implement putting url
+  }
+);
+
+router.all("*", () => error(404, "not found"));
+
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    return router.handle(request, env, ctx);
+  },
 };
